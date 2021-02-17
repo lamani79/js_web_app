84d32165ac298b27e6db28c2367fc298621163e3
Khelil_Yasser <lmutsuha79@gmail.com>
2021-02-17 21:28:12 +0100

    adding more function

--- color generator app/main.js
+++ color generator app/main.js
@@ -2,6 +2,8 @@ let but = document.getElementById("generator_but");
 let body = document.querySelector("body");
 let hexa = "01234561BCDEF" //all hexa code can be in color code
 let span = document.getElementById("hexa_color_text"); // the hexa color code span in html
+let copy_but = document.getElementById("copy");
+
 
 
 but.addEventListener('click',change_background);
@@ -22,5 +24,74 @@ function change_background(){
     body.style.backgroundColor = color;
     span.innerText = color;
     color = "#"; // reinitialize the value of color to '#'
+    copy_done();
+
+    
+
   
 };
+
+copy_but.addEventListener('click',function(){
+
+      let done = document.getElementById("done");
+    console.log(done.style)
+});
+
+
+function copy_color(){
+
+    let text_created = document.createElement("textarea");
+    text_created.value = span.textContent;
+    document.body.appendChild(text_created);
+    text_created.select()
+    document.execCommand('copy');
+    text_created.remove();
+
+};
+
+
+
+
+
+function copy_done(){
+
+    let copy_done_text = document.createElement("span");
+    copy_done_text.setAttribute("id","done");
+    let copy_done_text_style =  document.createElement("style");
+    copy_done_text.innerHTML = `
+    Copied!
+
+    `;
+    copy_done_text_style.innerHTML = `
+    #done {
+        backgroundColor: red;
+        position: absolute;
+        content: "copied";
+        background-color: white;
+        color: black;
+        font-weight: bold;
+        transition: opacity 0.3s, visibility 0.3s , top 0.3s;
+        top: O;
+        right: 0px;
+        width: 100%;
+        z-index: 1;
+      }
+      
+
+    `;
+    copy_but.appendChild(copy_done_text);
+     document.head.appendChild(copy_done_text_style);
+     var delayInMilliseconds = 2000; //1 second
+
+    setTimeout(function() {
+    copy_done_text.remove();
+    copy_done_text_style.remove();
+    }, delayInMilliseconds);
+    
+
+    
+     
+    };
+    
+
+  
